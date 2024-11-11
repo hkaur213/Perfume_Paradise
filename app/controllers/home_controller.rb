62a3127e0c7910d1641d42830a4e31afd6529666
Harleen Kaur <hharleenkaur@academic.rrc.ca>
2024-11-11 17:45:35 -0600

    Front Page created for the project

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+    def index
+      # Add any logic needed for the Home page
+    end
+  
+    def about
+      # Add any logic needed for the About page
+    end
+  end
+  
